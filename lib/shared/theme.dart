665cb8666ee27da79477d76b75b7d1473aafcdf1
DonzKrieg <iromdhon2004@gmail.com>
2025-10-14 12:22:53 +0700

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPositiveColor = Color(0xFF328e6e);
+Color kNegativeColor = Color(0xFFdc2525);
+Color kBlackColor = Color(0xFF000000);
+Color kWhiteColor = Color(0xFFFFFFFF);
